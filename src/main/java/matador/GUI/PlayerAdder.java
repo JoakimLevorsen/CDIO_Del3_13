6e845b66ee469a77e744bd11c28e19247d6486e8
Erlend Tyrmi <erlend@tyrmi.com>
2018-11-19 15:59:12 +0100

    setOwned method added in UIManager

--- src/main/java/matador/GUI/PlayerAdder.java
+++ src/main/java/matador/GUI/PlayerAdder.java
@@ -7,11 +7,12 @@ import gui_main.GUI;
 import java.awt.*;
 
 class PlayerAdder {
+    static Color[] colors = {Color.yellow, Color.RED, Color.blue, Color.green};;
+
     static GUI_Player[]  add(int numOfPlayers, GUI wGui, int defaultBalance) {
         // Make 3 arrays to build players from, then makes 'numOfPlayers' players with unique color. Max 4.
         GUI_Player[] players = new GUI_Player[numOfPlayers];
         GUI_Car[] cars = new GUI_Car[numOfPlayers];
-        Color[] colors = {Color.yellow, Color.RED, Color.blue, Color.green};
 
         for (int i = 0; i < players.length; i++) {
             cars[i] = new GUI_Car(colors[i], Color.black, GUI_Car.Type.UFO, GUI_Car.Pattern.FILL);
@@ -21,5 +22,10 @@ class PlayerAdder {
             wGui.getFields()[0].setCar(players[i], true);
         }
         return players;
+    }
+
+    static Color getColors(int x){
+        return colors[x];
+
     }
 }
