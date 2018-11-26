7e61f0f1882f4d16df8e0893493f4d3e24d8b4a1
Joakim Levorsen <joakim.levorsen@icloud.com>
2018-11-26 14:54:54 +0100

    ChanceCard super klasse

--- src/main/java/matador/cards/ChanceCard.java
+++ src/main/java/matador/cards/ChanceCard.java
@@ -1,6 +1,20 @@
 package matador.cards;
 
+import org.json.*;
+
+import matador.JSONKeys;
+
 abstract public class ChanceCard {
+
+    final String title;
+
+    public ChanceCard(JSONObject cardData) throws JSONException {
+        try {
+            this.title = cardData.getString(JSONKeys.TITLE);
+        } catch (Exception e) {
+            throw new JSONException("Read from JSON failed, check formatting.");
+        }
+    }
 
     abstract public void process();
 
