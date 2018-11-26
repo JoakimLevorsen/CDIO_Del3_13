dfaa5907e6d52b272509063779af8067586be6a8
Cecilie Drejer <ceciliek.drejer@gmail.com>
2018-11-26 14:39:27 +0100

    JSON edits

--- src/main/java/matador/GUI/BoardBuilder.java
+++ src/main/java/matador/GUI/BoardBuilder.java
@@ -37,7 +37,7 @@ class BoardBuilder {
                 int spaceType = spaces.getJSONObject(i).getInt(JSONKeys.TYPE);
                 String image = spaces.getJSONObject(i).getString(JSONKeys.IMAGE);
                 String title = spaces.getJSONObject(i).getString(JSONKeys.NAME);
-                String rent = spaces.getJSONObject(i).getString(JSONKeys.COST_STRING);
+                String rent = spaces.getJSONObject(i).getString(JSONKeys.VALUE_STRING);
                 String description = spaces.getJSONObject(i).getString(JSONKeys.MESSAGE);
                 Color BG_COLOR = getColor(spaces.getJSONObject(i).getString(JSONKeys.COLOR));
                 Color FG_COLOR = getColor(spaces.getJSONObject(i).getString(JSONKeys.TEXT_COLOR));
@@ -48,7 +48,7 @@ class BoardBuilder {
                     case 2: board[i] = new GUI_Street(title, rent, description, rent, BG_COLOR, FG_COLOR); break;
                     case 3: board[i] = new GUI_Jail(image, title, description, rent, BG_COLOR, FG_COLOR); break;
                     case 4: board[i] = new GUI_Jail(image, title, description, rent, BG_COLOR, FG_COLOR); break;
-                    case 5: board[i] = new GUI_Chance(title, description, rent, BG_COLOR, FG_COLOR); break;
+                    case 5: board[i] = new GUI_Chance(title, "", description, BG_COLOR, FG_COLOR); break;
                     default: throw new JSONException("Read from JSON failed, check formatting.");
                 }
             }
