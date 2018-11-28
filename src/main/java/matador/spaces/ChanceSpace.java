2b9624c748afecc001a3c9cf0de89f6521dfcea7
Cecilie Drejer <ceciliek.drejer@gmail.com>
2018-11-28 11:27:52 +0100

    Removed redundancies in ChanceSpace

--- src/main/java/matador/spaces/ChanceSpace.java
+++ src/main/java/matador/spaces/ChanceSpace.java
@@ -2,13 +2,10 @@ package matador.spaces;
 
 import org.json.JSONObject;
 import matador.game.*;
-import matador.cards.*;
 
 public class ChanceSpace extends Space {
-    private CardManager manager;
 
     public ChanceSpace(JSONObject data, CardManager manager) {
         super(data);
-        this.manager = manager;
     }
 }
