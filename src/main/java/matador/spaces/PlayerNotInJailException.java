de5dab09fdfe41f90c1161d7341136facd1adf93
Joakim Levorsen <joakim.levorsen@icloud.com>
2018-11-28 11:43:16 +0100

    Exception warnings scilenced

--- src/main/java/matador/spaces/PlayerNotInJailException.java
+++ src/main/java/matador/spaces/PlayerNotInJailException.java
@@ -1,6 +1,10 @@
 package matador.spaces;
 
 public class PlayerNotInJailException extends Exception {
+
+    // This can fuck the entire program up if serialized, so DO NOT SERIALIZE THIS CLASS, THIS VARIABLE WAS ONLY DEFINED TO SCILENCE A WARNING
+    static final long serialVersionUID = 0xBADBEEF;
+
     public PlayerNotInJailException(String message) {
         super(message);
     }
